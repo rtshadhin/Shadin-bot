a0924d8cbfd08b787643b2650cb8be0a1ded028d
rtshadhin <99165825+rtshadhin@users.noreply.github.com>
2024-04-26 15:39:34 +0600

    Update leave.js

--- scripts/events/leave.js
+++ scripts/events/leave.js
@@ -13,14 +13,14 @@ module.exports.run = async function({ api, event, Users, Threads }) {
 	const { threadID } = event;
 	const data = global.data.threadData.get(parseInt(threadID)) || (await Threads.getData(threadID)).data;
 	const name = global.data.userName.get(event.logMessageData.leftParticipantFbId) || await Users.getNameUser(event.logMessageData.leftParticipantFbId);
-	const type = (event.author == event.logMessageData.leftParticipantFbId) ? " কিরে ভাই গুরুপ কি তোর বাপের ইচ্ছা হলে বারিয়ে যাবি" : "Kicked by আমার এডমিনদুস্ত";
+	const type = (event.author == event.logMessageData.leftParticipantFbId) ? "এই এডমিন আমার শালাটারে কিক কেন দিলে হ্যা তোমার নামে থানায় কেচ দিবো" : "Kicked by আমার বউ ";
 	const path = join(__dirname, "nayan", "leaveGif");
 	const gifPath = join(path, `l.gif`);
 	var msg, formPush
 
 	if (existsSync(path)) mkdirSync(path, { recursive: true });
 
-	(typeof data.customLeave == "undefined") ? msg = "কিরেশালা {name} এই গুরুপটা তোর নানার যে ইচ্ছা হলে বারিয়ে যাবি .\n\n{type} " : msg = data.customLeave;
+	(typeof data.customLeave == "undefined") ? msg = "কিরেশালা {name} শর ভাই শর আমি বোরট মানুষ হলে তোরে একবারে 😋 .\n\n{type} " : msg = data.customLeave;
 	msg = msg.replace(/\{name}/g, name).replace(/\{type}/g, type);
 
 	if (existsSync(gifPath)) formPush = { body: msg, attachment: createReadStream(gifPath) }
